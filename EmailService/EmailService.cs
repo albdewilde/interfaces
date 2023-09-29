6703e5870c7a07049b9fdaeeffe3754b9c4ea3b3
albdewilde <adewilde@fcms.io>
2023-09-29 14:00:44 +0200

    Remove empty constructor

--- EmailService/EmailService.cs
+++ EmailService/EmailService.cs
@@ -2,8 +2,6 @@ namespace Interface.EmailService;
 
 class EmailService : IEmailService
 {
-    public EmailService() {}
-
     public void SendEmail()
     {
         Console.WriteLine("Sending an e-mail");
